c632d932273b15971432729cac17180c04564233
etawong <134659889+etawong@users.noreply.github.com>
2023-06-30 17:19:09 -0700

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -12,4 +12,10 @@ def call(String stageName){
      {
        sh "mvn clean deploy"
      }
+else if ("${stageName}" == "tomcat")
+    {
+    deploy adapters: [tomcat9(credentialsId: 'Tomcat-credentials', path: '', url: 'http://35.89.133.135:8177')], contextPath: null, war: 'target/*.war'
+    sh "echo application successfully deployed into the tomcat application server"
+    }
 }
+
